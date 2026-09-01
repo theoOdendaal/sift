850f9eaa94c6d6e0675ad41a925029342ee45412
theo <scrawl-86bee@icloud.com>
2026-09-01 18:30:51 +0200

    Various changes, none of which are really important

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 // specifically to properly parse arch news.
 // https://github.com/magiclen/html-escape
 
+
+use std::io::{Read, Write};
+
 fn _bbc_news_content() -> Result<String, Box<dyn std::error::Error>> {
     let mut response = ureq::get("https://feeds.bbci.co.uk/news/rss.xml?edition=uk").call()?;
     let body: String = response.body_mut().read_to_string()?;
@@ -25,8 +28,33 @@ fn _arch_rss_content() -> Result<String, Box<dyn std::error::Error>> {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //let content = _arch_rss_content()?;
-    let content = _bbc_news_content()?;
+    
+    // Entering raw mode
+    let _raw_guard = sift::interface::RawModeGuard::enable()?;
+
+    sift::interface::draw_box(5, 3, 40, 10, " Integrated TUI ")?;
+
+    let mut stdout = std::io::stdout();
+    write!(
+        stdout,
+        "\x1B[5;7H\x1B[1;32mRaw mode + Alternate Screen active!\x1B[0m"
+    )?;
+    write!(stdout, "\x1B[7;7HPress 'q' to exit...")?;
+    stdout.flush()?;
+
+    let mut stdin = std::io::stdin();
+    let mut buf = [0u8; 1];
+
+    // Read bytes instantly without needing Enter
+    loop {
+        if stdin.read_exact(&mut buf).is_ok() && buf[0] == b'q' {
+            break;
+        }
+    }
+
+    /*
+    let content = _arch_rss_content()?;
+    //let content = _bbc_news_content()?;
 
     let mut xml_tokenizer = sift::xml::tokens::Tokenizer::new(&content);
 
@@ -44,7 +72,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     //let body = rss_feed.items[0].follow_link()?;
     //std::fs::write("test.html", &body)?;
 
-    /*
     let body = std::fs::read_to_string("test.html")?;
 
     let mut html_tokenizer = sift::html::tokens::HtmlTokenizer::new(&body);
@@ -55,7 +82,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             break;
         }
         //tokens.push(token);
-    }*/
+    }
+    */
 
     Ok(())
 }
