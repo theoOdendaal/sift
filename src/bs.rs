d43eddb82fa1dbe643de7bb947ab563156f53d2c
theo <scrawl-86bee@icloud.com>
2026-09-18 20:08:20 +0200

    I believe I am satisfied with bs(byte search/scan), for now

--- src/bs.rs
+++ src/bs.rs
@@ -1,3 +1,5 @@
+// byte scanner
+
 #[repr(u8)]
 #[derive(Debug, PartialEq)]
 pub enum Error {
@@ -26,13 +28,14 @@ impl<'a> From<&'a [u8]> for Scanner<'a> {
     }
 }
 
-// All of the below consume fn assume
-// that the byte at self.pos represent the start
-// of the slice that has to be consumed.
+/// All of the below consume fn assume
+/// that the byte at self.pos represent the start
+/// of the slice that has to be consumed. The fn's will
+/// also not consume the byte that broke the consumption.
 impl<'a> Scanner<'a> {
     
     #[inline]
-    pub fn consume_until(&mut self, stop: impl Fn(u8) -> bool) -> Option<&'a [u8]> {
+    fn consume_until(&mut self, stop: impl Fn(u8) -> bool) -> Option<&'a [u8]> {
         let start = self.pos;
         let len = self.bytes[start..].iter().position(|&b| stop(b))?;
         self.pos = start + len;
@@ -40,7 +43,15 @@ impl<'a> Scanner<'a> {
     }
 
     #[inline]
-    pub fn consume_until_or_eof(&mut self, stop: impl Fn(u8) -> bool) -> &'a [u8] {
+    fn consume_until_sequence(&mut self, delim: &[u8]) -> Option<&'a [u8]> {
+        let start = self.pos;
+        let len = self.bytes[start..].windows(delim.len()).position(|w| w == delim)?;
+        self.pos = start + len;
+        Some(&self.bytes[start..self.pos])
+    }
+
+    #[inline]
+    fn consume_until_or_eof(&mut self, stop: impl Fn(u8) -> bool) -> &'a [u8] {
         let start = self.pos;
         let len = match self.bytes[start..].iter().position(|&b| stop(b)) {
             Some(idx) => idx,
@@ -70,9 +81,6 @@ impl<'a> Scanner<'a> {
             Err(Error::UnexpectedEndOfFile)
         }
     }
-    
-    //#[inline]
-    //fn consume_processing_instruction_data(&mut self) -> Result<&'a [u8], Error>
     
     #[inline]
     pub fn consume_attribute_name(&mut self) -> Result<&'a [u8], Error> {
@@ -105,14 +113,23 @@ impl<'a> Scanner<'a> {
         self.consume_until_or_eof(|b| b == b'<')
     }
 
-    //#[inline]
-    //fn consume_comment(&mut self) -> Result<&'a [u8], Error>
+    #[inline]
+    pub fn consume_comment(&mut self) -> Result<&'a [u8], Error> {
+        if let Some(value) = self.consume_until_sequence(b"-->") {
+            Ok(value)
+        } else {
+            Err(Error::UnexpectedEndOfFile)
+        }
+    }
 
-    //#[inline]
-    //fn consume_cdata(&mut self) -> Result<&'a [u8], Error>
-
-    //#[inline]
-    //fn consume_entity_declaration(&mut self) -> Result<&'a [u8], Error>
+    #[inline]
+    pub fn consume_cdata(&mut self) -> Result<&'a [u8], Error> {
+        if let Some(value) = self.consume_until_sequence(b"]]>") {
+            Ok(value)
+        } else {
+            Err(Error::UnexpectedEndOfFile)
+        }
+    }
 
 }
 
@@ -123,7 +140,36 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_advance_past_whitespaces1() {}
+    fn test_advance_past_whitespaces1() {
+        let input = b" theo";
+        let mut scanner = Scanner::from(input.as_slice());
+        scanner.advance_past_whitespaces();
+        assert_eq!(scanner.pos, 1)
+    }
+
+    #[test]
+    fn test_advance_past_whitespaces2() {
+        let input = b"  theo";
+        let mut scanner = Scanner::from(input.as_slice());
+        scanner.advance_past_whitespaces();
+        assert_eq!(scanner.pos, 2)
+    }
+
+    #[test]
+    fn test_advance_past_whitespaces3() {
+        let input = b"theo";
+        let mut scanner = Scanner::from(input.as_slice());
+        scanner.advance_past_whitespaces();
+        assert_eq!(scanner.pos, 0)
+    }
+
+    #[test]
+    fn test_advance_past_whitespaces4() {
+        let input = b"      \t      \n    \r  ";
+        let mut scanner = Scanner::from(input.as_slice());
+        scanner.advance_past_whitespaces();
+        assert_eq!(scanner.pos, input.len())
+    }
 
     #[test]
     fn test_consume_tag_name1() {
@@ -150,16 +196,13 @@ mod tests {
     }
     
     #[test]
-    fn test_consume_processing_instruction_data() {}
+    fn test_consume_attribute_name1() {}
     
     #[test]
-    fn test_consume_attribute_name() {}
-    
-    #[test]
-    fn test_consume_quoted_attribute_value() {}
+    fn test_consume_quoted_attribute_value1() {}
 
     #[test]
-    fn test_consume_unquoted_attribute_value() {}
+    fn test_consume_unquoted_attribute_value1() {}
     
     #[test]
     fn test_consume_text1() {
@@ -178,12 +221,35 @@ mod tests {
     }
     
     #[test]
-    fn test_consume_comment() {}
+    fn test_consume_comment1() {
+        let input = b"hello world, my name is theo-->";
+        let mut scanner = Scanner::from(input.as_slice());
+        let slice = scanner.consume_comment();
+        assert_eq!(slice, Ok(b"hello world, my name is theo".as_slice()))
+    }
+
+    #[test]
+    fn test_consume_comment2() {
+        let input = b"hello world, my name is theo--";
+        let mut scanner = Scanner::from(input.as_slice());
+        let slice = scanner.consume_comment();
+        assert_eq!(slice, Err(Error::UnexpectedEndOfFile))
+    }
     
     #[test]
-    fn test_consume_cdata() {}
-    
+    fn test_consume_cdata1() {
+        let input = b"hello world, my name is theo]]>";
+        let mut scanner = Scanner::from(input.as_slice());
+        let slice = scanner.consume_cdata();
+        assert_eq!(slice, Ok(b"hello world, my name is theo".as_slice()))
+    }
+
     #[test]
-    fn test_consume_entity_declaration() {}
+    fn test_consume_cdata2() {
+        let input = b"hello world, my name is theo] ]>";
+        let mut scanner = Scanner::from(input.as_slice());
+        let slice = scanner.consume_cdata();
+        assert_eq!(slice, Err(Error::UnexpectedEndOfFile))
+    }
 
 }
