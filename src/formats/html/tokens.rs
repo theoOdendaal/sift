aac9a9f1cedfa2109b9877e673159157a86f70ae
theo <scrawl-86bee@icloud.com>
2026-09-21 08:28:23 +0200

    Continued with the html side

--- src/formats/html/tokens.rs
+++ src/formats/html/tokens.rs
@@ -13,6 +13,18 @@ pub enum HtmlToken<'a> {
     Text(&'a [u8]),
 
     StartTag(&'a [u8]),
+
+    EndTag(&'a [u8]),
+
+    TagEnd {
+        self_closing: bool,
+    },
+
+    Comment(&'a [u8]),
+
+    CData(&'a [u8]),
+
+    DocType(&'a [u8]),
 }
 
 pub struct HtmlTokenizer<'a> {
@@ -32,6 +44,11 @@ impl<'a> std::fmt::Display for HtmlToken<'a> {
         match self {
             Self::Text(a) => write!(f, "Text({})", String::from_utf8_lossy(a)),
             Self::StartTag(a) => write!(f, "StartTag({})", String::from_utf8_lossy(a)),
+            Self::EndTag(a) => write!(f, "EndTag({})", String::from_utf8_lossy(a)),
+            Self::TagEnd { self_closing } => write!(f, "TagEnd(self_closing={})", self_closing),
+            Self::Comment(a) => write!(f, "Comment({})", String::from_utf8_lossy(a)),
+            Self::CData(a) => write!(f, "CData({})", String::from_utf8_lossy(a)),
+            Self::DocType(a) => write!(f, "DocType({})", String::from_utf8_lossy(a)),
         }
     }
 } 
@@ -53,21 +70,51 @@ impl<'a> HtmlTokenizer<'a> {
         self.scanner.consume_byte();
 
         if self.scanner.is_byte(b'!') {
+            
             if self.scanner.starts_with(b"<!--") {
-
+                self.scanner.consume_n_bytes(4);
+                match self.scanner.consume_comment() {
+                    Some(comment) => return Some(Ok(HtmlToken::Comment(comment))),
+                    None => return Some(Err(self.error(ErrorKind::UnexpectedEndOfFile))),
+                }
             }
 
             if self.scanner.starts_with(b"<![CDATA[") {
-
+                self.scanner.consume_n_bytes(9);
+                match self.scanner.consume_cdata() {
+                    Some(comment) => return Some(Ok(HtmlToken::CData(comment))),
+                    None => return Some(Err(self.error(ErrorKind::UnexpectedEndOfFile))),
+                }
             }
 
             if self.scanner.starts_with(b"<!DOCTYPE") {
+                self.scanner.consume_n_bytes(9);
+                if let Some(value) = self.scanner.consume_until(|b| b == b'>') {
+                    self.scanner.consume_byte();
+                    return Some(Ok(HtmlToken::DocType(value)));
+                }
+                return Some(Err(self.error(ErrorKind::UnexpectedEndOfFile)));
 
             }
         }
 
-
         if self.scanner.starts_with(b"</") {
+            self.scanner.advance_past_whitespaces();
+            if self.scanner.is_eof() {
+                return Some(Err(self.error(ErrorKind::UnexpectedEndOfFile)));
+            }
+
+            let name = match self.scanner.consume_tag_name() {
+                Some(name) => name,
+                None => return  Some(Err(self.error(ErrorKind::UnexpectedEndOfFile))),
+            };
+            
+            // FIXME: actually consume attributes in the end tag.
+            if self.scanner.consume_until(|b| b == b'>').is_none() {
+                return Some(Err(self.error(ErrorKind::UnexpectedEndOfFile)));
+            }
+            self.scanner.consume_byte();
+            return Some(Ok(HtmlToken::EndTag(name)));
 
         }
 
@@ -80,6 +127,30 @@ impl<'a> HtmlTokenizer<'a> {
         self.state = HtmlState::AfterStartTagName;
         Some(Ok(HtmlToken::StartTag(tag_name)))
     }
+
+    fn next_after_start_tag_name(&mut self) -> Option<Result<HtmlToken<'a>, Error>> {
+        self.scanner.advance_past_whitespaces();
+        if self.scanner.is_eof() {
+            return Some(Err(self.error(ErrorKind::UnexpectedEndOfFile)));
+        }
+
+        if self.scanner.starts_with(b"/>") {
+            self.scanner.consume_n_bytes(2);
+            self.state = HtmlState::Data;
+
+            Some(Ok(HtmlToken::TagEnd { self_closing: true }))
+        } else if self.scanner.is_byte(b'>') {
+            self.scanner.consume_byte();
+            self.state = HtmlState::Data;
+            Some(Ok(HtmlToken::TagEnd { self_closing: false }))
+
+        } else {
+            self.scanner.consume_byte();
+            Some(Ok(HtmlToken::Text(b"")))
+            //Some(self.consume_attribute())
+        }
+    }
+
 }
 
 impl<'a> Iterator for HtmlTokenizer<'a> {
@@ -92,7 +163,7 @@ impl<'a> Iterator for HtmlTokenizer<'a> {
 
         match self.state {
             HtmlState::Data => self.next_data(),
-            HtmlState::AfterStartTagName => unimplemented!("AfterStartTagName")
+            HtmlState::AfterStartTagName => self.next_after_start_tag_name(), 
         } 
     }
 }
