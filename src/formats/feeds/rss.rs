4513caea78013044e2817e3e851212e11b9e95fb
theo <scrawl-86bee@icloud.com>
2026-09-23 09:14:06 +0200

    Oops, looks like I also need to be able to parse dublin core metadata. But this seems easy enough, my rss parse module is however becoming a but cramped

--- src/formats/feeds/rss.rs
+++ src/formats/feeds/rss.rs
@@ -6,12 +6,15 @@ use crate::formats::xml::tokens::XmlToken;
 #[derive(Debug)]
 pub enum Error {
     Utf8Parse(std::str::Utf8Error),
+    Other(String),
 }
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Utf8Parse(e) => write!(f, "{}", e),
+            Self::Utf8Parse(e) => write!(f, "{e}"),
+            Self::Other(e) => write!(f, "{e}")
+
         }
     }
 }
@@ -23,6 +26,24 @@ impl From<std::str::Utf8Error> for Error {
         Self::Utf8Parse(value)
     }
 }
+
+#[derive(Debug)]
+struct ByteParserError;
+
+impl std::fmt::Display for ByteParserError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("Byte parse error")
+    }
+}
+
+impl std::error::Error for ByteParserError {}
+
+impl From<ByteParserError> for Error {
+    fn from(value: ByteParserError) -> Self {
+        Self::Other("Byte parse error".into())
+    }
+}
+
 
 #[derive(Debug, PartialEq, Eq)]
 enum XmlParseState {
@@ -59,6 +80,8 @@ enum RssTag {
     Enclosure,
     Guid,
     Source,
+
+    DublinCore(DublinCoreLegacyNamespace),
 }
 
 #[derive(Debug, Default)]
@@ -112,6 +135,51 @@ pub struct Feed<'a> {
     namespaces: Vec<(Cow<'a, str>, Cow<'a, str>)>,
     pub channel: Option<Channel<'a>>,
     pub items: Vec<Item<'a>>,
+}
+
+#[repr(u8)]
+#[derive(Debug)]
+enum DublinCoreLegacyNamespace {
+    Contributor,
+    Coverage,
+    Creator,
+    Date,
+    Description,
+    Format,
+    Identifier,
+    Language,
+    Publisher,
+    Relation,
+    Rights,
+    Subject,
+    Title,
+    Type,
+}
+
+
+impl<'a> TryFrom<&'a [u8]> for DublinCoreLegacyNamespace {
+    type Error = ByteParserError;
+
+    fn try_from(value: &'a [u8]) -> Result<Self, Self::Error> {
+        match value {
+            b"dc:contributor" => Ok(Self::Contributor),
+            b"dc:coverage" => Ok(Self::Coverage),
+            b"dc:creator" => Ok(Self::Creator),
+            b"dc:date" => Ok(Self::Date),
+            b"dc:description" => Ok(Self::Description),
+            b"dc:format" => Ok(Self::Format),
+            b"dc:identifier" => Ok(Self::Identifier),
+            b"dc:language" => Ok(Self::Language),
+            b"dc:publisher" => Ok(Self::Publisher),
+            b"dc:relation" => Ok(Self::Relation),
+            b"dc:rights" => Ok(Self::Rights),
+            b"dc:subject" => Ok(Self::Subject),
+            b"dc:title" => Ok(Self::Title),
+            b"dc:type" => Ok(Self::Type),
+            _ => Err(ByteParserError),
+
+        }
+    }
 }
 
 impl<'a> std::fmt::Display for Channel<'a> {
@@ -532,7 +600,11 @@ impl<'a> RssParser<'a> {
                         self.current_tag = Some(RssTag::Source);
                     }
 
-                    _ => {}
+                    _ => {
+                        if name.starts_with(b"dc:") {
+                            self.current_tag = Some(RssTag::DublinCore(DublinCoreLegacyNamespace::try_from(name)?));
+                        } 
+                    }
                 },
 
                 XmlToken::EndTag(name) => match name {
